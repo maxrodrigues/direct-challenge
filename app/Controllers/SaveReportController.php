84cd1773ed647d25ec80fd5d768aced509f89e1f
Max Rodrigues <maxuel.rodrigues@gmail.com>
2025-08-08 00:59:09 -0300

    feat: add validation and REST response

--- app/Controllers/SaveReportController.php
+++ app/Controllers/SaveReportController.php
@@ -5,12 +5,27 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\Report;
 use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\API\ResponseTrait;
+use Exception;
 
 class SaveReportController extends BaseController
 {
+    use ResponseTrait;
     public function index()
     {
         try {
+            $rules = [
+                'patient' => 'required|string|min_length[6]',
+                'birthDate' => 'required|string|valid_date[d/m/Y]',
+                'exam' => 'required|string',
+            ];
+
+            $data = $this->request->getPost(array_keys($rules));
+
+            if(! $this->validateData($data, $rules)) {
+                return $this->failValidationErrors($this->validator->listErrors());
+            }
+
             $report = new Report();
             $save = $report->insert([
                 'patient' => $this->request->getPost('patient'),
@@ -22,9 +37,10 @@ class SaveReportController extends BaseController
                 throw new \Exception("Failed to save report");
             }
 
-            return redirect()->to('/');
-        } catch (\Throwable $e) {
-            die($e->getMessage());
+            return $this->respondCreated('Created report successfully');
+
+        } catch (Exception $exception) {
+            $this->fail($exception->getMessage());
         }
     }
 }
